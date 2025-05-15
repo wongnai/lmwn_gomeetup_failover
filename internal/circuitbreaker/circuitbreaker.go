b2f6b156cd2ef83a5908af1052fd5643bd44b31e
IamPhon <sittiphon.chitmongkolthong@gmail.com>
2025-05-16 00:06:34 +0700

    add clear example circuit breaker

--- internal/circuitbreaker/circuitbreaker.go
+++ internal/circuitbreaker/circuitbreaker.go
@@ -1,6 +1,7 @@
 package circuitbreaker
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/sony/gobreaker"
@@ -13,9 +14,17 @@ type CircuitBreaker struct {
 func NewCircuitBreaker() *CircuitBreaker {
 	cb := gobreaker.NewCircuitBreaker(gobreaker.Settings{
 		Name:        "ServiceCircuitBreaker",
-		MaxRequests: 1,
-		Interval:    10 * time.Second,
-		Timeout:     5 * time.Second,
+		MaxRequests: 3,                // Allowed in half-open state
+		Interval:    10 * time.Second, // Rolling window to reset counts
+		Timeout:     5 * time.Second,  // Duration to wait before switching from open to half-open
+		ReadyToTrip: func(counts gobreaker.Counts) bool {
+			// Trip the breaker if the failure ratio is > 60% over 5+ requests
+			failRatio := float64(counts.TotalFailures) / float64(counts.Requests)
+			return counts.Requests >= 5 && failRatio >= 0.6
+		},
+		OnStateChange: func(name string, from, to gobreaker.State) {
+			fmt.Printf("Circuit %s changed state from %s to %s\n", name, from.String(), to.String())
+		},
 	})
 	return &CircuitBreaker{cb: cb}
 }
